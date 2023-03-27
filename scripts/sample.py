3b0771dbedf1c42998f84a0625d4100efb24dc78
Tim Tröndle <tim.troendle@usys.ethz.ch>
2023-03-27 18:09:55 +0200

    Fix population statistics

--- scripts/sample.py
+++ scripts/sample.py
@@ -11,15 +11,29 @@ POP_25_29_ALL = "DEM_4.19"
 POP_30_39_ALL = "DEM_4.22"
 POP_40_49_ALL = "DEM_4.25"
 POP_50_64_ALL = "DEM_4.28"
+POP_50_59_ALL = "DEM_3.19"
+POP_60_69_ALL = "DEM_3.22"
+POP_70_79_ALL = "DEM_3.25"
+POP_80_PLUS_ALL = "DEM_3.28"
 POP_65_74_ALL = "DEM_4.31"
 POP_75_PLUS_ALL = "DEM_4.34"
+POP_ABOVE_18_ALL = [
+    POP_18_24_ALL, POP_25_29_ALL, POP_30_39_ALL, POP_40_49_ALL,
+    POP_50_64_ALL, POP_65_74_ALL, POP_75_PLUS_ALL
+]
+POP_ABOVE_18_MALE = [
+    "DEM_4.17", "DEM_4.20", "DEM_4.23", "DEM_4.26", "DEM_4.29", "DEM_4.32", "DEM_4.35"
+]
+POP_ABOVE_18_FEMALE = [
+    "DEM_4.18", "DEM_4.21", "DEM_4.24", "DEM_4.27", "DEM_4.30", "DEM_4.33", "DEM_4.36"
+]
 
 
 def sample_vs_population(path_to_sample: str, path_to_population: str, path_to_output: str):
     pop = read_population(path_to_population)
     sample = read_sample(path_to_sample)
 
-    total_pop = pop[POP_ALL].sum()
+    total_pop = pop[POP_ABOVE_18_ALL].sum().sum()
     total_sample = sample.lfdn.count()
 
     table = pd.DataFrame(
@@ -35,7 +49,7 @@ def sample_vs_population(path_to_sample: str, path_to_population: str, path_to_o
         data={
             "sample": [
                 sample[sample["gen"] == "male"].lfdn.count() / total_sample,
-                sample[sample["gen"] == "feamle"].lfdn.count() / total_sample,
+                sample[sample["gen"] == "female"].lfdn.count() / total_sample,
                 sample[sample["age"] == "<30"].lfdn.count() / total_sample,
                 sample[sample["age"] == "30-39"].lfdn.count() / total_sample,
                 sample[sample["age"] == "40-49"].lfdn.count() / total_sample,
@@ -43,13 +57,13 @@ def sample_vs_population(path_to_sample: str, path_to_population: str, path_to_o
                 sample[sample["age"] == "60+"].lfdn.count() / total_sample,
             ],
             "population": [
-                pop[POP_ALL_MALE].sum() / total_pop,
-                pop[POP_ALL_FEMALE].sum() / total_pop,
+                pop[POP_ABOVE_18_MALE].sum().sum() / total_pop,
+                pop[POP_ABOVE_18_FEMALE].sum().sum() / total_pop,
                 pop.loc[:, [POP_18_24_ALL, POP_25_29_ALL]].sum().sum() / total_pop,
                 pop[POP_30_39_ALL].sum() / total_pop,
                 pop[POP_40_49_ALL].sum() / total_pop,
-                pop[POP_50_64_ALL].sum() / total_pop,
-                pop.loc[:, [POP_65_74_ALL, POP_75_PLUS_ALL]].sum().sum() / total_pop,
+                pop[POP_50_59_ALL].sum() / total_pop,
+                pop.loc[:, [POP_60_69_ALL, POP_70_79_ALL, POP_80_PLUS_ALL]].sum().sum() / total_pop,
             ]
         }
     )
